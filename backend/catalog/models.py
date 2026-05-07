5c52de6424ebb94f7cd5c0eb175282c383deb427
autograph1 <ldragon837@gmail.com>
2026-05-07 21:42:29 +0300

    catalog

---
+++ backend/catalog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from users.models import User
+class Product(models.Model):
+    url = models.CharField(max_length=100)
+    needed_price = models.IntegerField()
+    current_price = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
