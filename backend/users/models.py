51231ad35dc40e789d1c0690bb4ee6ec06237677
autograph1 <ldragon837@gmail.com>
2026-05-07 16:17:03 +0300

    admin

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+
+class User(AbstractUser):
+    telegram_id = models.BigIntegerField(unique=True, blank=True, null=True)
+
+
