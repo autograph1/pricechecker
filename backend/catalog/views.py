d757583121b0d0c832f56a68edb337ea80c7343e
autograph1 <ldragon837@gmail.com>
2026-05-18 20:37:35 +0300

    post request

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -4,8 +4,20 @@ from .serializers import ProductSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(['GET'],['POST'])
 def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products,many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        data = request.data 
+        serializer = ProductSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
