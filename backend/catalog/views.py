eb5abaac5f0863dd66155c8264d77a5f6e965907
autograph1 <ldragon837@gmail.com>
2026-05-29 19:54:46 +0300

    authenfication

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,7 +1,15 @@
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 class ProductListSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return Product.objects.filter(owner=self.request.user)
