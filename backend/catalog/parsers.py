86ec4ccd1506885f5906d8bb48cddb2548d0598b
autograph1 <ldragon837@gmail.com>
2026-06-02 15:37:07 +0300

    parsers

--- backend/catalog/parsers.py
+++ backend/catalog/parsers.py
@@ -1,19 +1,4 @@
-from playwright.sync_api import sync_playwright
-
-
+import requests
 def parse_ozon(url):
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-
-        page = browser.new_page()
-
-        page.goto(url)
-
-        input("Пройди капчу и нажми Enter в терминале...")
-
-        print("TITLE:")
-        print(page.title())
-
-        input("Нажми Enter для закрытия браузера...")
-
-        browser.close()
+    response = requests.get(url)
+    print(response.status_code)
