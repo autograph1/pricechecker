5376a5e4734e1f03bf79a5e0a401fd0a43167e32
autograph1 <ldragon837@gmail.com>
2026-06-04 20:16:30 +0300

    connected django and parser

--- backend/catalog/management/commands/check_prices.py
+++ backend/catalog/management/commands/check_prices.py
@@ -1,7 +1,9 @@
 from django.core.management import BaseCommand
 from catalog.models import Product
-
+from catalog.parsers import parse_ozon
 class Command(BaseCommand):
     def handle(self, *args, **options):
         products = Product.objects.all()
-        print(products.count())
+        for product in products:
+            data = parse_ozon(product.url)
+            print(data)
