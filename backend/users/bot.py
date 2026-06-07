ec169c2e64f0ab85b12598317cd2f2ed8c0def06
autograph1 <ldragon837@gmail.com>
2026-06-07 18:58:06 +0300

    connection to tg

--- backend/users/bot.py
+++ backend/users/bot.py
@@ -1,12 +1,22 @@
 import telebot
 
+from users.models import User
 from django.conf import settings
+
 
 bot = telebot.TeleBot(settings.BOT_TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+
+    user, created = User.objects.get_or_create(
+        telegram_id=message.from_user.id,
+        defaults={
+            "username": f"tg_{message.from_user.id}"
+        }
+    )
+
     bot.send_message(
         message.chat.id,
         "Привет, я бот для отслеживания цен"
